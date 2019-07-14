9dc80b2cd2715f8e1e3c0e84289e717730e0899b
Ethan Kent <ethan.kent@amount.com>
2019-07-14 15:18:33 -0500

    MVP

--- main.rb
+++ main.rb
@@ -4,6 +4,22 @@ require 'pry'
 require 'set'
 
 class Sudoku
+  def print_game(game)
+    puts TOP_ROW
+
+    game.each_slice(9).each_with_index do |row, index|
+      print SIDE_THICK
+      interior = row.each_slice(3).map do |chunk|
+        chunk.map { |e| e || '·' }.join(' ')
+      end.to_a.join(SIDE_THIN)
+      print interior
+      puts SIDE_THICK
+      puts INNER_ROW if [2, 5].include?(index)
+    end
+
+    puts BOTTOM_ROW
+  end
+
   protected
 
   DIMENSION = 9
@@ -22,28 +38,11 @@ class Sudoku
   INNER_TOP = '┯'
   INNER_RIGHT = '┨'
   TOP_ROW =    '┏━━━━━┯━━━━━┯━━━━━┓'
-              # ┃1 1 1 1 1 1 1 1 1┃
   INNER_ROW =  '┠─────┼─────┼─────┨'
   BOTTOM_ROW = '┗━━━━━┷━━━━━┷━━━━━┛'
 
   # TOP_ROW = CORNER_TOP_LEFT + TOP_THICK * 17 + CORNER_TOP_RIGHT
   # BOTTOM_ROW = CORNER_BOTTOM_LEFT + BOTTOM_THICK * 17 + CORNER_BOTTOM_RIGHT
-
-  def print_game(game)
-    puts TOP_ROW
-
-    game.each_slice(9).each_with_index do |row, index|
-      print SIDE_THICK
-      interior = row.each_slice(3).map do |chunk|
-        chunk.map { |e| e || "·" }.join(" ")
-      end.to_a.join(SIDE_THIN)
-      print interior
-      puts SIDE_THICK
-      puts INNER_ROW if index == 2 || index == 5
-    end
-
-    puts BOTTOM_ROW
-  end
 
   def row_iterator
     @row_iterator ||= (0..8).map { |row| (0..8).map { |col| row * 9 + col } }
@@ -107,11 +106,11 @@ class SudokuSolver < Sudoku
   end
 
   def solve(game)
-    puts print_game(game)
+    system 'clear'
+    print_game(game)
     return [game] if rules.win?(game)
     return [] if rules.complete?(game)
 
-    binding.pry
     next_frontier(game).flat_map { |g| solve(g) }
   end
 
@@ -127,30 +126,32 @@ class SudokuSolver < Sudoku
     end
   end
 
-	def next_frontier(game)
+  def next_frontier(game)
     iter_kind, chunk_index = choose_chunk(game)
     iter = send(iter_kind)[chunk_index]
     chunk = iter.map { |i| game[i] }
     valid_next_states(chunk, game, iter)
-	end
+  end
 
+  # Find the game chunk that is not entirely filled but as close as possible to
+  # being filled.
   def choose_chunk(game)
-    iter_kind = :square_iterator
-    chunk_index = send(iter_kind)
-                  .each_with_index
-                  .reduce([0, nil]) do |(best_count, best_index), (iter, index)|
-      unfilled_count = DIMENSION - iter.map { |i| game[i] }.compact.length
+    %i[row_iterator square_iterator col_iterator].shuffle.map do |iter_kind|
+      count, chunk_index = send(iter_kind)
+                           .each_with_index
+                           .reduce([DIMENSION, nil]) do |(best_count, best_index), (iter, index)|
+        unfilled_count = DIMENSION - iter.map { |i| game[i] }.compact.length
 
-      if unfilled_count &&
-         unfilled_count < DIMENSION &&
-         unfilled_count < best_count
-        [unfilled_count, index]
-      else
-        [best_count, best_index]
+        if unfilled_count.positive? &&
+           unfilled_count < best_count
+          [unfilled_count, index]
+        else
+          [best_count, best_index]
+        end
       end
-    end.first
 
-    [iter_kind, chunk_index] if chunk_index
+      [iter_kind, chunk_index, count]
+    end.min { |a, b| a.last <=> b.last }.tap { |x| [x[0], x[1]] }
   end
 
   def unused_permutations(chunk)
@@ -167,17 +168,28 @@ class SudokuSolver < Sudoku
   end
 end
 
-GAME = [nil, nil, 5, nil, 1, nil, nil, nil, nil,
-        nil, 2, 1, nil, nil, nil, nil, 8, nil,
-        nil, nil, nil, 8, nil, 4, nil, 1, 6,
-        nil, nil, 9, nil, 2, nil, 1, nil, nil,
-        2, nil, nil, 1, nil, 7, nil, nil, nil,
-        nil, nil, 7, nil, 6, nil, 8, nil, nil,
-        9, 8, nil, 4, nil, 2, nil, nil, nil,
-        nil, 3, nil, nil, nil, nil, 4, 7, nil,
-        nil, nil, nil, nil, 9, nil, 6, nil, nil].freeze
+# GAME = [nil, nil, 5, nil, 1, nil, nil, nil, nil,
+#         nil, 2, 1, nil, nil, nil, nil, 8, nil,
+#         nil, nil, nil, 8, nil, 4, nil, 1, 6,
+#         nil, nil, 9, nil, 2, nil, 1, nil, nil,
+#         2, nil, nil, 1, nil, 7, nil, nil, nil,
+#         nil, nil, 7, nil, 6, nil, 8, nil, nil,
+#         9, 8, nil, 4, nil, 2, nil, nil, nil,
+#         nil, 3, nil, nil, nil, nil, 4, 7, nil,
+#         nil, nil, nil, nil, 9, nil, 6, nil, nil].freeze
 
-SudokuSolver.new.solve(GAME)
+GAME = [8, nil, nil, nil, nil, nil, nil, nil, nil,
+        nil, nil, 3, 6, nil, nil, nil, nil, nil,
+        nil,   7, nil, nil, 9, nil, 2, nil, nil,
+        nil,   5, nil, nil, nil, 7, nil, nil, nil,
+        nil, nil, nil, nil, 4, 5, 7, nil, nil,
+        nil, nil, nil, 1, nil, nil, nil, 3, nil,
+        nil, nil,   1, nil, nil, nil, nil, 6, 8,
+        nil, nil,   8, 5, nil, nil, nil, 1, nil,
+        nil, 9, nil, nil, nil, nil, 4, nil, nil].freeze
+
+s = SudokuSolver.new
+s.print_game(s.solve(GAME).first)
 
 __END__
 
@@ -190,3 +202,18 @@ SOLVED_GAME = [8, 4, 5, 2, 1, 6, 7, 9, 3,
                9, 8, 6, 4, 7, 2, 3, 5, 1,
                5, 3, 2, 6, 8, 1, 4, 7, 9,
                1, 7, 4, 5, 9, 3, 6, 2, 8].freeze
+
+https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
+┏━━━━━┯━━━━━┯━━━━━┓
+┃8 1 2│7 5 3│6 4 9┃
+┃9 4 3│6 8 2│1 7 5┃
+┃6 7 5│4 9 1│2 8 3┃
+┠─────┼─────┼─────┨
+┃1 5 4│2 3 7│8 9 6┃
+┃3 6 9│8 4 5│7 2 1┃
+┃2 8 7│1 6 9│5 3 4┃
+┠─────┼─────┼─────┨
+┃5 2 1│9 7 4│3 6 8┃
+┃4 3 8│5 2 6│9 1 7┃
+┃7 9 6│3 1 8│4 5 2┃
+┗━━━━━┷━━━━━┷━━━━━┛
